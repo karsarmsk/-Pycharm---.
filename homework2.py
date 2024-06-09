19e6c602d9bd7e03a81c1cbce3fe805ff95fdf23
Сергей Карасев <karasev-s@internet.ru>
2024-06-09 13:22:54 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+completed_tasks = 12 # Количество выполненных ДЗ
+time_spent = 1.5 # Количество затраченных часов
+Course_name = 'Python' # Название курса
+time_for_one_task = time_spent / completed_tasks # Время на одно задание
+print('Курс:',Course_name,', всего задач:',completed_tasks,', затрачено часов:',
+      time_spent,', среднее время выполнения',time_for_one_task,'часа') # Вывод на экран
